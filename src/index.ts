75c0bc19145c15631f8e2bae490499937fa6c4dd
Joel Wang <joel.wang@autodesk.com>
2020-03-12 17:53:04 +0800

    I think I'm done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const ACCENTED_VOWELS = ['á', 'é', 'í', 'ó', 'ú'];
 const NON_ACCENTED_VOWELS = ['a', 'e', 'i', 'o', 'u', 'ü'];
+const STRESSED_VOWELS = new Set([...ACCENTED_VOWELS, 'a', 'o', 'e']);
 const ALL_VOWELS = [...ACCENTED_VOWELS, ...NON_ACCENTED_VOWELS];
 const WEAK_VOWELS = ['i', 'u', 'ü'];
 const CONSONANT_BLENDS = [
@@ -18,24 +19,45 @@ const CONSONANT_BLENDS = [
   'tl',
 ];
 
-// TODO: What is "Y"?
-// TODO: Vowel clusters?
-
-export const DIAGRAPHS = ['ch', 'll', 'rr'];
+const DIAGRAPHS = ['ch', 'll', 'rr'];
 
 function isVowel(char: string) {
-  return ALL_VOWELS.includes(char);
+  return ALL_VOWELS.includes(char.toLowerCase());
 }
 
-export function isWeakVowel(char: string) {
-  return WEAK_VOWELS.includes(char);
+function isWeakVowel(char: string) {
+  return WEAK_VOWELS.includes(char.toLowerCase());
 }
 
-export function isAccentedVowel(char: string) {
-  return ACCENTED_VOWELS.includes(char);
+const SAME_VOWEL_SETS = [
+  new Set(['a', 'á']),
+  new Set(['o', 'ó']),
+  new Set(['e', 'é']),
+  new Set(['i', 'í']),
+  new Set(['u', 'ú', 'ü']),
+];
+
+function isHiatus(a: string, b: string) {
+  const aL = a.toLowerCase();
+  const bL = b.toLowerCase();
+  return (
+    // same vowel
+    SAME_VOWEL_SETS.some(s => s.has(aL) && s.has(bL)) ||
+    // or both stressed
+    (STRESSED_VOWELS.has(aL) && STRESSED_VOWELS.has(bL))
+  );
 }
 
-interface Syllable {
+function makesTriphthong(a: string, b: string) {
+  const vowels = a + b;
+  return (
+    isWeakVowel(vowels[0]) &&
+    STRESSED_VOWELS.has(vowels[1].toLowerCase()) &&
+    isWeakVowel(vowels[2])
+  );
+}
+
+export interface Syllable {
   onset: string;
   nucleus: string;
   coda: string;
@@ -43,7 +65,7 @@ interface Syllable {
 
 export const syllable2Str = (syllable: Syllable) => {
   return syllable.onset + syllable.nucleus + syllable.coda;
-}
+};
 
 enum Position {
   None,
@@ -56,17 +78,18 @@ export class Word {
   public word: string;
   public length: number;
   public syllables: Syllable[];
+
   constructor(word: string) {
     this.word = word;
     this.length = word.length;
     this.syllables = [];
-    this.parse();
+    this.toSyllables();
   }
-  private parse() {
+
+  private toSyllables() {
     let index = 0;
     let position = Position.None;
-    let syllable: Syllable = {onset: '', nucleus: '', coda: ''};
-    // const nextsyllable: Syllable = {onset: '', nucleus: '', coda: ''};
+    let syllable: Syllable = { onset: '', nucleus: '', coda: '' };
     while (true) {
       const char = this.word[index];
       if (!isVowel(char)) {
@@ -74,30 +97,97 @@ export class Word {
           position = Position.atOnset;
           syllable.onset = syllable.onset + char;
         } else if (position === Position.atNucleus) {
-          position = Position.atCoda;
-          syllable.coda = syllable.coda + char;
+          if (index === this.length - 1 && char === 'y') {
+            syllable.nucleus = syllable.nucleus + char;
+          } else if (char === 'h') {
+            index += 1;
+            const nextChar = this.word[index];
+            if (isVowel(nextChar)) {
+              if (isHiatus(syllable.nucleus, nextChar)) {
+                this.syllables.push(syllable);
+                syllable = { onset: char, nucleus: nextChar, coda: '' };
+                position = Position.atNucleus;
+              } else {
+                index += 1;
+                const nnextChar = this.word[index];
+                if (isVowel(nnextChar)) {
+                  if (makesTriphthong(syllable.nucleus + nextChar, nnextChar)) {
+                    // Could this happen?
+                  } else {
+                    this.syllables.push(syllable);
+                    syllable = {
+                      onset: char,
+                      nucleus: nextChar + nnextChar,
+                      coda: '',
+                    };
+                    position = Position.atNucleus;
+                  }
+                } else {
+                  syllable.nucleus = syllable.nucleus + char + nextChar;
+                  syllable.coda = nnextChar;
+                  position = Position.atCoda;
+                }
+              }
+            } else {
+              syllable.coda = char;
+              this.syllables.push(syllable);
+              syllable = { onset: nextChar, nucleus: '', coda: '' };
+              position = Position.atOnset;
+            }
+          } else {
+            syllable.coda = syllable.coda + char;
+            position = Position.atCoda;
+          }
         } else if (position === Position.atCoda) {
           position = Position.atCoda;
           syllable.coda = syllable.coda + char;
         }
       } else {
         // is vowel
-        if (
-          position === Position.None ||
-          position === Position.atOnset ||
-          position === Position.atNucleus
-        ) {
+        if (position === Position.None || position === Position.atOnset) {
           position = Position.atNucleus;
           syllable.nucleus = syllable.nucleus + char;
+        } else if (position === Position.atNucleus) {
+          if (syllable.nucleus.length === 1) {
+            if (isHiatus(char, syllable.nucleus)) {
+              this.syllables.push(syllable);
+              syllable = { onset: '', nucleus: char, coda: '' };
+              position = Position.atNucleus;
+            } else {
+              syllable.nucleus = syllable.nucleus + char;
+              position = Position.atNucleus;
+            }
+          } else if (syllable.nucleus.length === 2) {
+            if (makesTriphthong(syllable.nucleus, char)) {
+              syllable.nucleus = syllable.nucleus + char;
+              position = Position.atNucleus;
+            } else {
+              const lastNucleus = syllable.nucleus[1];
+              if (isWeakVowel(lastNucleus)) {
+                this.syllables.push({
+                  ...syllable,
+                  nucleus: syllable.nucleus[0],
+                });
+                syllable = { onset: '', nucleus: lastNucleus + char, coda: '' };
+              } else {
+                this.syllables.push(syllable);
+                syllable = { onset: '', nucleus: char, coda: '' };
+              }
+              position = Position.atNucleus;
+            }
+          }
         } else if (position === Position.atCoda) {
           if (syllable.coda.length === 1) {
             const temp = syllable.coda;
             syllable.coda = '';
             this.syllables.push(syllable);
-            syllable = {onset: temp, nucleus: char, coda: ''};
+            syllable = { onset: temp, nucleus: char, coda: '' };
           } else if (syllable.coda.length === 2) {
             let temp: string;
-            if (CONSONANT_BLENDS.includes(syllable.coda) || DIAGRAPHS.includes(syllable.coda)) {
+            if (
+              CONSONANT_BLENDS.includes(syllable.coda) ||
+              DIAGRAPHS.includes(syllable.coda)
+            ) {
               temp = syllable.coda;
               syllable.coda = '';
             } else {
@@ -105,17 +195,17 @@ export class Word {
               syllable.coda = syllable.coda[0];
             }
             this.syllables.push(syllable);
-            syllable = {onset: temp, nucleus: char, coda: ''};
+            syllable = { onset: temp, nucleus: char, coda: '' };
           } else if (syllable.coda.length === 3) {
             const temp = syllable.coda.slice(1, 3);
             syllable.coda = syllable.coda[0];
             this.syllables.push(syllable);
-            syllable = {onset: temp, nucleus: char, coda: ''};
+            syllable = { onset: temp, nucleus: char, coda: '' };
           } else if (syllable.coda.length === 4) {
             const temp = syllable.coda.slice(2, 4);
             syllable.coda = syllable.coda.slice(0, 2);
             this.syllables.push(syllable);
-            syllable = {onset: temp, nucleus: char, coda: ''};
+            syllable = { onset: temp, nucleus: char, coda: '' };
           }
           position = Position.atNucleus;
         }
---
+++ test/index.test.ts
@@ -0,0 +1,207 @@
+import { Word, syllable2Str } from '../src';
+
+const DIPTONGOS = [
+  'pai-sa-je',
+  'pei-ne',
+  'an-droi-de',
+  'pau-sa',
+  'feu-do',
+  'es-ta-dou-ni-den-se',
+  'su-cia',
+  'tie-rra',
+  'pio-jo',
+  're-cua',
+  'puer-ta',
+  're-si-duo',
+  'ciu-dad',
+  'bui-tre',
+  'muy',
+  'viu-da',
+];
+
+export const HIATOS = [
+  // e
+  'le-e',
+  'pa-se-é',
+  'se-a',
+  'te-a-tro',
+  'cre-ó',
+  'fe-o',
+  're-í',
+  're-ú-ne',
+  // a
+  'ca-e',
+  'a-é-re-o',
+  'a-za-har',
+  'na-o',
+  'ca-o-ba',
+  'pa-ís',
+  'ba-úl',
+  // o
+  'ro-e',
+  'No-é',
+  'bo-a-to',
+  'Sa-mo-a',
+  'lo-ó',
+  'zo-o',
+  'o-ír',
+  'No-ú-me-no',
+  // i
+  'rí-e',
+  'fi-lo-so-fí-a',
+  'rí-o',
+  'chi-i-ta',
+  // u
+  'li-cú-e',
+  'pú-a',
+  'a-cen-tú-o',
+  'du-un-vi-ro',
+];
+
+// TODO: oa, ea no son hiatos en Mexico
+
+const TRIPTONGOS = [
+  'A-bre-viáis',
+  'A-bre-viéis',
+  'Ac-tuáis',
+  'A-huau-tle',
+  'Buey',
+  'Ca-ma-güey',
+  'Ciais',
+  'Crieis',
+  'Cuai-mas',
+  'Dioi-co',
+  'Guay-mas',
+  'Huey-pox-tla',
+  'Miau',
+  // No son triptongos
+  'lim-pia-ú-ñas',
+  'vi-ví-ais',
+];
+
+// Most from https://github.com/vic/silabas.js
+const TEST_DATA = [
+  'va-te',
+  'su-yo',
+  'ár-bol',
+  'pa-la-bra',
+  'es-to-cól-mo',
+  'i-dí-li-co',
+  'i-rru-ma-ción',
+  'i-ne-fa-ble',
+  'hi-po-pó-ta-mo',
+  'ay',
+  'hay',
+  'ma-guey',
+  'a-buha-do',
+  'ac-mé',
+  'hai-ga',
+  'mam-po-rre-ro',
+  'mur-cié-la-go',
+  'ple-io-tro-pí-a',
+  'Ab-yec-ción',
+  'A-he-rro-jar',
+  'güe-ro',
+  'a-ve-ri-guáis',
+  'U-ru-guay',
+  'huí-a',
+  'az-ca-pot-zal-co',
+  'va-he-e',
+  'pte-ra-sau-rio',
+];
+
+const WITH_H = ['a-ni-hi-lar', 'ma-ri-hua-na'];
+
+describe('blah', () => {
+  it('vc', () => {
+    const word = new Word('la');
+    expect(word.length).toEqual(2);
+    expect(word.syllables).toEqual([{ coda: '', nucleus: 'a', onset: 'l' }]);
+  });
+  it('cv', () => {
+    const word = new Word('al');
+    expect(word.length).toEqual(2);
+    expect(word.syllables).toEqual([{ coda: 'l', nucleus: 'a', onset: '' }]);
+  });
+  it('cvy', () => {
+    const word = new Word('doy');
+    expect(word.syllables).toEqual([{ coda: '', nucleus: 'oy', onset: 'd' }]);
+  });
+  it('cvv', () => {
+    const word = new Word('duo');
+    expect(word.syllables).toEqual([{ coda: '', nucleus: 'uo', onset: 'd' }]);
+  });
+  it('cvvy', () => {
+    const word = new Word('buey');
+    expect(word.syllables).toEqual([{ coda: '', nucleus: 'uey', onset: 'b' }]);
+  });
+  it('cvvy', () => {
+    const word = new Word('buey');
+    expect(word.syllables).toEqual([{ coda: '', nucleus: 'uey', onset: 'b' }]);
+  });
+  it('vcvc', () => {
+    const word = new Word('lalo');
+    expect(word.syllables.map(syllable2Str)).toEqual(['la', 'lo']);
+  });
+  it('vcv', () => {
+    const word = new Word('ala');
+    expect(word.syllables.map(syllable2Str)).toEqual(['a', 'la']);
+  });
+  it('vccv', () => {
+    const word = new Word('alto');
+    expect(word.syllables.map(syllable2Str)).toEqual(['al', 'to']);
+  });
+  it('vccv, digraph', () => {
+    const word = new Word('allá');
+    expect(word.syllables.map(syllable2Str)).toEqual(['a', 'llá']);
+  });
+  it('vccv, compound', () => {
+    const word = new Word('extra');
+    expect(word.syllables.map(syllable2Str)).toEqual(['ex', 'tra']);
+  });
+
+  TEST_DATA.map(word => {
+    const combined = word.replace(/-/g, '');
+    const split = word.split('-');
+    it(`${combined} => ${word}`, () => {
+      const word = new Word(combined);
+      expect(word.syllables.map(syllable2Str)).toEqual(split);
+    });
+  });
+
+  DIPTONGOS.map(word => {
+    const combined = word.replace(/-/g, '');
+    const split = word.split('-');
+    it(`${combined} => ${word}`, () => {
+      const word = new Word(combined);
+      expect(word.syllables.map(syllable2Str)).toEqual(split);
+    });
+  });
+
+  TRIPTONGOS.map(word => {
+    const combined = word.replace(/-/g, '');
+    const split = word.split('-');
+    it(`${combined} => ${word}`, () => {
+      const word = new Word(combined);
+      expect(word.syllables.map(syllable2Str)).toEqual(split);
+    });
+  });
+
+  HIATOS.map(word => {
+    const combined = word.replace(/-/g, '');
+    const split = word.split('-');
+    it(`${combined} => ${word}`, () => {
+      const word = new Word(combined);
+      expect(word.syllables.map(syllable2Str)).toEqual(split);
+    });
+  });
+
+  WITH_H.map(word => {
+    const combined = word.replace(/-/g, '');
+    const split = word.split('-');
+    fit(`${combined} => ${word}`, () => {
+      const word = new Word(combined);
+      expect(word.syllables.map(syllable2Str)).toEqual(split);
+    });
+  });
+});
